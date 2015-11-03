2ea206509aa914626ba35a557ab3916faf3d7c2a
Raim Interiano <rai.interiano@gmail.com>
2015-11-03 11:21:13 -0600

    Change of template

--- HackPro/Models/Login.cs
+++ HackPro/Models/Login.cs
@@ -5,7 +5,7 @@ namespace HackPro.Models
     public class Login
     {
         [Required]
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Correo de formato inválido.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Correo de formato inválido.")]
         public string email { get; set; }
 
         [Required(ErrorMessage = "El campo de password es requerido.")]        
